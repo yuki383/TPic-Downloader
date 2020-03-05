c13620482c3c654e8ce88ca045668ea5136540b6
yuu383 <yuukikokubo.pg@gmail.com>
2020-03-05 19:55:34 +0900

    moved main.go to cmd/tpicer

--- cmd/tpicer/main.go
+++ cmd/tpicer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"tpic-fetcher/pkg/arguments"
 	"tpic-fetcher/pkg/files"
@@ -12,20 +13,21 @@ func main() {
 	args, err := arguments.ParseFlags()
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	pics, err := pictures.New(args.Name, args.URL)
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	size, err := files.New("data", pics.Name, pics.Data)
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	fmt.Printf("save successed bytes: %d\n", size)
+	os.Exit(0)
 }
